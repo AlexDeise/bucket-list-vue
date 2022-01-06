f0761de8d447d3a2c023385cc6119835f925aa27
AlexDeise <alexmdeise@gmail.com>
2022-01-06 11:55:09 -0500

    initial upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <v-row>
+    <v-card>
+      <v-img
+      src="../assets/deadpool.jpeg"
+      >
+      <v-card-title></v-card-title>
+      </v-img>
+    </v-card>
+    <v-col>
+      <v-text-field
+      label="Movie Title"> </v-text-field>
+    </v-col>
+    <v-col>
+      <v-text-field
+      label="Movie Title"> </v-text-field>
+    </v-col>
+  </v-row>
+</div>
+</template>
+<script>
+  export default {
+   }
+</script>
